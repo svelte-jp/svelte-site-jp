207163ef1d73b2ad974f8dd855d93434c42b4028
tomoam <29677552+tomoam@users.noreply.github.com>
2021-02-09 18:35:57 +0900

    translate: add support for japanese in API Docs

--- src/routes/docs/index.json.js
+++ src/routes/docs/index.json.js
@@ -1,11 +1,13 @@
 import send from '@polka/send';
 import get_sections from './_sections.js';
+import { getCookie } from '../../modules/cookie.js'
 
 let json;
 
 export function get(req, res) {
 	if (!json || process.env.NODE_ENV !== 'production') {
-		json = get_sections();
+		const locale = getCookie('locale', req.headers.cookie);
+		json = get_sections(locale);
 	}
 
 	send(res, 200, json);
